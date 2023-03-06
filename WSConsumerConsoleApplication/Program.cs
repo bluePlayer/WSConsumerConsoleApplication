7d6dc378320ceb367a135fa4c8edca42d475ede8
bluePlayer <vlzakar@gmail.com>
2023-03-06 17:46:50 +0100

    Апликација за вежбање веб сервиси

---
+++ WSConsumerConsoleApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace WSConsumerConsoleApplication
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            MyWebServiceConsumer consumer = new MyWebServiceConsumer();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
